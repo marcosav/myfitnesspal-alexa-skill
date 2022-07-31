b6a59330e487a3260565e3e0ff16775a87f55d13
marcosav <marcosav2010@gmail.com>
2022-07-31 13:14:29 +0200

    Default constructor for MealConfig

--- src/main/kotlin/com/gmail/marcosav2010/config/meals/MealConfig.kt
+++ src/main/kotlin/com/gmail/marcosav2010/config/meals/MealConfig.kt
@@ -2,7 +2,7 @@ package com.gmail.marcosav2010.config.meals
 
 import com.gmail.marcosav2010.domain.Meal
 
-data class MealConfig(val meals: List<Meal>) {
+data class MealConfig(val meals: List<Meal> = emptyList()) {
 
     fun byName(name: String) = meals.find { it.name == name }
 }
